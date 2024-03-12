3a1f55ca23659b84d26205d2ed9a9fc1bc1ea766
MkGodh <fenryrvla@yandex.ru>
2024-03-12 12:19:51 +0300

    Добавьте файлы проекта.

---
+++ BioLab/BioLab.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstring>
+
+
+int main(){
+
+        using namespace std;
+
+        setlocale(LC_ALL, "Rus");
+
+        unsigned long long bacteria;
+        int antibiotic;
+        cout << "Введите колличество бактерий: "; cin >> bacteria;
+        cout << "Ввелите колличество антибиотика: "; cin >> antibiotic;
+        int rezBio = antibiotic * 10; // 10
+        int hours = 1;
+
+        do {
+            bacteria = (bacteria * 2) - rezBio;
+            cout << "После " << hours << " часа бактерий осталось " << bacteria << "\n";
+            rezBio--;
+            hours++;
+            if (bacteria < rezBio / 2) {
+                bacteria *= 0;
+                cout << "После " << hours << " часа бактерий осталось " << bacteria << "\n";
+                break;
+            }
+
+        } while (bacteria > 0 && rezBio > 0);
+   }
+
